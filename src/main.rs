bf5bce7bcc26d6b368106df2437bb65a92ae4d97
Yeabsirashimelis <shimelisyeabsiragithub@gmail.com>
2025-12-12 02:36:25 -0800

    file compression finished

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use std::{
+    env::args,
+    fs::File,
+    io::{BufReader, copy},
+    time::Instant,
+};
+
+use flate2::{Compression, write::GzEncoder};
+
 fn main() {
-    println!("Hello, world!");
+    if args().len() != 3 {
+        eprintln!("Usage: `source` `target`");
+        return;
+    }
+
+    let mut input = BufReader::new(File::open(args().nth(1).unwrap()).unwrap());
+    let output = args().nth(2).unwrap();
+
+    let output_file_path = if output.contains(".") {
+        let output_file_path = output.split(".").collect::<Vec<&str>>()[0];
+        format!("{}.zip", output_file_path)
+    } else {
+        format!("{}.zip", output)
+    };
+
+    println!("{output_file_path}");
+
+    let output = File::create(output_file_path).unwrap();
+    let mut encoder = GzEncoder::new(output, Compression::default());
+    let start = Instant::now();
+    copy(&mut input, &mut encoder).unwrap();
+    let output = encoder.finish().unwrap();
+    println!(
+        "Source len: {:?}",
+        input.get_ref().metadata().unwrap().len()
+    );
+
+    println!("Target len: {:?}", output.metadata().unwrap().len());
+    println!("Elapsed: {:?}", start.elapsed())
 }
